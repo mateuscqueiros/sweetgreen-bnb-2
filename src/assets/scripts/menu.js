07721e36d9d34d173c1a0ffabf5032ab3e001c6b
Mateus Queirós <mateuscqueiros@gmail.com>
2024-09-19 11:58:49 -0300

    Parte 2 - 19/09

---
+++ src/assets/scripts/menu.js
@@ -0,0 +1,17 @@
+// 1 - Localizar os elementos que abrem e fecham o menu (querySelector, getElementById)
+// 2 - Adicionar os eventos de click (addEventListener)
+// 3 - Adicionar/remover a classe em cada evento (element.classList.add)
+
+const navElement = document.querySelector('.menu .container nav');
+const openMenuButton = document.getElementById('open-menu');
+const closeMenuButton = document.getElementById('close-menu');
+
+openMenuButton.addEventListener('click', () => {
+  navElement.classList.add('open');
+});
+
+closeMenuButton.addEventListener('click', () => {
+  navElement.classList.remove('open');
+});
+
+console.log(navElement);
